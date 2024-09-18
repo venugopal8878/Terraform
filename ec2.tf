90879c25bc9c93b30760e15df1a70d484e6b49fb
venugopal <mallavallivenu@gmail.com>
2024-09-18 11:40:52 +0530

    terraform

--- ec2.tf
+++ ec2.tf
@@ -18,4 +18,8 @@ resource "aws_security_group" "allow_ssh_terraform" {
         cidr_blocks      = ["0.0.0.0/0"] #allow from everyone
         ipv6_cidr_blocks = ["::/0"]
     }
+
+    tags= {
+          name = "venntf"
+    }
 }
--- provider.tf
+++ provider.tf
@@ -1,15 +1,14 @@
 terraform {
   required_providers {
     aws = {
-
-        source = "hashicorp/aws"
-        version = "5.66.0"
-    
+      source = "hashicorp/aws"
+      version = "5.66.0"
     }
   }
 }
 
 provider "aws" {
-    #configuration options
+  # Configuration options
     region = "us-east-1"
 }
+
