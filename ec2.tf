16b815209a0d368515e988dc5e18e302ed7f7080
venugopal <mallavallivenu@gmail.com>
2024-09-18 10:33:55 +0530

    terraform

--- ec2.tf
+++ ec2.tf
@@ -1,24 +1,20 @@
-resource "aws_security_group" "allow ssh" { 
-   name = "allow_sssh"
-   description = "allow port  number 22"
+resource "aws_security_group" "allow_ssh_terraform" {
+    name        = "allow_sshh" #allow_ssh is already there in my account
+    description = "Allow port number 22 for SSH access"
 
-
-  egress {
-       from_port  = 0
-       to_port    = 0
-       protocol   = "-1"
-       cidr_blocks = ["0.0.0.0/0"]
-       ipv6_cidr_blocks = ["::/0"]
-  }
-
-   ingress {
-
-       from_port = 22
-       to_port = 22
-       protocol = "tcp
-       cidr_blocks = ["0.0.0.0/0"] #allow from every one
-       ipv6_cidr_blocks = ["::/0"]
+    # usually we allow everything in egress
+    egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
     }
 
-}
-
+    ingress {
+        from_port        = 22
+        to_port          = 22
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"] #allow from everyone
+        ipv6_cidr_blocks = ["::/0"]
+    }
