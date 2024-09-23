54d7d0c550723ec68b57fd191e7ceee7e9ee468e
venugopal <mallavallivenu@gmail.com>
2024-09-23 10:10:23 +0530

    terraform

--- ec2.tf
+++ ec2.tf
@@ -26,8 +26,8 @@ resource "aws_security_group" "allow_ssh_terraform" {
 
 resource "aws_instance"  "terraform"{
        ami_id= "ami-09c813fb71547fc4f"
-       instance_type: "t3.micro"
-       vpc_security_group_ids=[aws_security_group-allow_ssh_terraform.id]
+       instance_type="t3.micro"
+       vpc_security_group_ids=[aws_security_group.allow_ssh_terraform.id]
 
        tags {
 
