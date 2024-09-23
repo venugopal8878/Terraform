0706011220e1716ce3ee0f96cc6bb4a88b58b702
venugopal <mallavallivenu@gmail.com>
2024-09-23 10:09:02 +0530

    terraform

--- ec2.tf
+++ ec2.tf
@@ -20,6 +20,17 @@ resource "aws_security_group" "allow_ssh_terraform" {
     }
 
     tags= {
-          name = "venntf"
+          Name = "venntf"
     }
 }
+
+resource "aws_instance"  "terraform"{
+       ami_id= "ami-09c813fb71547fc4f"
+       instance_type: "t3.micro"
+       vpc_security_group_ids=[aws_security_group-allow_ssh_terraform.id]
+
+       tags {
+
+        Name= "terraform"
+       }
+ }
