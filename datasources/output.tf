698d307002f663af8ca1240f56e26bfc61a5293d
venugopal <mallavallivenu@gmail.com>
2024-09-29 10:26:33 +0530

    remotestates3

--- datasources/output.tf
+++ datasources/output.tf
@@ -1,8 +1,8 @@
-# output "aws_instance" {
-#     value = aws_instance.expense
-# }
-
-
-output "ami_id" {
-    value = data.aws_ami.expense.id
+output "aws_instance" {
+    value = aws_instance.expense
 }
+
+
+# output "ami_id" {
+#     value = data.aws_ami.expense.id
+# }
--- remote-state/provider.tf
+++ remote-state/provider.tf
@@ -7,10 +7,10 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "mytfremotestate"  #s3 bucket name
-    key = "venu.remotestate-demo" #just specify the key for we user different key for diffrentr repos and we don't use this for another repository
-    region = "us-east-1"
-    dynamodb_table = "venu"
+    bucket ="mytfremotestate"  #s3 bucket name
+    key ="venu.remotestate-demo" #just specify the key for we user different key for diffrentr repos and we don't use this for another repository
+    region ="us-east-1"
+    dynamodb_table ="venu"
   }
   
 }
